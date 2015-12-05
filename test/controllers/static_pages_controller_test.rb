fc421f58d43359def759c87ac4189eea344869d0
bernard laveaux <blaveaux@outlook.com>
2015-12-05 12:24:42 -0500

    update static_pages home and help, create initial about static page test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
+# Controller inferred by class name (ie StaticPages|ControllerTest)
 class StaticPagesControllerTest < ActionController::TestCase
+  # can specifically infer a controller by using
+  # tests StaticPagesController
+
   test "should get home" do
     get :home
     assert_response :success
@@ -8,6 +12,11 @@ class StaticPagesControllerTest < ActionController::TestCase
 
   test "should get help" do
     get :help
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
     assert_response :success
   end
 
