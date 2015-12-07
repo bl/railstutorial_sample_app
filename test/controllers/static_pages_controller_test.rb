1225b638ca6d54ba37c07d20aefccac57eabea5b
bernard laveaux <blaveaux@outlook.com>
2015-12-07 17:40:52 -0500

    Added static_pages Contact page and tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -27,4 +27,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | #{@base_title}"
   end
 
+  test "shoudl get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+
 end
