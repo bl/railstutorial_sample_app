941dd08a2880d662f0d4bffc73d0234b744f256b
bernard laveaux <blaveaux@outlook.com>
2015-12-22 02:21:57 -0500

    added user model tests & validation email & name fields, cleared user fixtures

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+  # block to execute before .save calls to bring email to lowercase
+  before_save { self.email = email.downwcase }
+
+  # call validates with attribute and optinos hash requiring presence on name field
+  validates :name, presence: true, length: { maximum: 50}
+  # valid email REGEX (accepting basic structure email formats)
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # validate email field to require presence
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX},
+                    uniqueness: { case_sensitive: false }
+
+  has_secure_password
 end
