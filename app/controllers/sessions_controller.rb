967c640a82e455cf4574bf691f30b550574c8754
bernard laveaux <blaveaux@outlook.com>
2015-12-24 21:27:53 -0500

    improve persistence session log-in test coverage

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,14 @@ class SessionsController < ApplicationController
 
   def create
     # retrieve user matching email
-    user = User.find_by(email: params[:session][:email].downcase)
+    @user = User.find_by(email: params[:session][:email].downcase)
     # verify user exists and password submitted matches
-    if user && user.authenticate(params[:session][:password])
+    if @user && @user.authenticate(params[:session][:password])
       # log the user in and redirect to the user's show page
-      log_in user
+      log_in @user
       # store user remember token
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       # create an error message
       flash.now[:danger] = "Invalid email/password combination"
--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -13,5 +13,6 @@ class SessionsHelperTest < ActionView::TestCase
   
   test "current_user returns nil when remember digest is wrong" do
     @user.update_attribute(:remember_digest, User.digest(User.new_token))
+    assert_nil current_user
   end
 end
