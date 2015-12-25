1fde2fe6506f46ee1d3ffc056d9ed2d8edaf24d7
bernard laveaux <blaveaux@outlook.com>
2015-12-24 19:05:51 -0500

    implemented cookie based permanent remember me, resolved two bugs related to multiple windows and cookie based sessions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,13 +6,36 @@ module SessionsHelper
 
   # logs out the current user
   def log_out
-    session.delete(:user_id);
-    @current_user = nil;
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  # remembers a user in a persistent session
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # forgets a persistent session
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 
   # returns the current logged-in user (if any)
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if (user && user.authenticated?(cookies[:remember_token]))
+          log_in user
+          @current_user = user
+      end
+    end
   end
 
   # returns true if the user is logged in, false otherwise
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  # virtual (non-ActiveRecord) attribute to store remember token
+  attr_accessor :remember_token
   # block to execute before .save calls to bring email to lowercase
   before_save { email.downcase! }
 
@@ -18,5 +20,26 @@ class User < ActiveRecord::Base
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # returns a random token (used as cookie token)
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # returns true if the given token matches the digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
