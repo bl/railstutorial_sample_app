06c81ec7448596064ec209cab88aacb7bceebfe8
bernard laveaux <blaveaux@outlook.com>
2015-12-30 22:07:18 -0500

    refactored root page logged-in and non-logged-in views into partials, expanded test coverage of image uploads

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -9,15 +9,18 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
     # invalid submission
     assert_no_difference 'Micropost.count' do
       post microposts_path, micropost: { content: "" }
     end
     # valid submission
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, micropost: { content: content }
+      post microposts_path, micropost: { content: content, picture: picture }
     end
+    assert assigns(:micropost).picture?
     assert_redirected_to root_url
     follow_redirect!
     # delete a post
@@ -29,5 +32,21 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     # visit a different user
     get user_path(users(:archer))
     assert_select 'a',  text: 'delete', count: 0
+  end
+
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    plural_micropost = 'micropost'
+    plural_micropost += 's' if @user.microposts.count > 1
+    assert_match "#{@user.microposts.count} #{plural_micropost}", response.body
+    # user with zero microposts
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match "1 micropost", response.body
   end
 end
