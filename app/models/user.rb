0860a98aaf5fb1d9299ec9b8b1f85d5362683492
bernard laveaux <blaveaux@outlook.com>
2016-01-02 17:34:15 -0500

    implemented feed displaying microposts from all followed users

--- app/models/user.rb
+++ app/models/user.rb
@@ -85,7 +85,10 @@ class User < ActiveRecord::Base
 
   # Defines a proto-feed
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
   # follows a user
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -96,4 +96,22 @@ class UserTest < ActiveSupport::TestCase
     chris.unfollow(archer)
     assert_not chris.following?(archer)
   end
+
+  test "feed should have the right posts" do
+    chris   = users(:chris)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # posts from the followed user
+    lana.microposts.each do |post_following|
+      assert chris.feed.include?(post_following)
+    end
+    # posts from self
+    chris.microposts.each do |post_self|
+      assert chris.feed.include?(post_self)
+    end
+    # posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not chris.feed.include?(post_unfollowed)
+    end
+  end
 end
